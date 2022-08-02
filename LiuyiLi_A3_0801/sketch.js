97bac416f110bfaf2127e852e307ac776fcaecde
LiuyiLi2000 <106932722+LiuyiLi2000@users.noreply.github.com>
2022-08-01 21:36:58 -0400

    Add files via upload

---
+++ LiuyiLi_A3_0801/sketch.js
@@ -0,0 +1,9 @@
+let dataServer;
+let pubKey = "pub-c-3aa89993-4234-4b05-b184-112207cb6750";
+let subKey = "sub-c-0f3876cc-016e-4d72-8f47-fc83b33b0f33";
+let secretKey = "sec-c-NGEzMDE1NTktODFlYy00MjM4LWJjYWYtZWMxZGQ5MDE4NzMy";
+
+let channelName = "history";
+
+let you;
+
